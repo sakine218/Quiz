51073f64a4744be9d6c9992f34da5be23760bdd3
sakine218 <snishibayashi218@gmail.com>
2019-04-10 09:24:23 +0900

    first commit

---
+++ Quiz/QuizViewController.swift
@@ -0,0 +1,92 @@
+//
+//  QuizViewController.swift
+//  Quiz
+//
+//  Created by litech on 2015/02/10.
+//  Copyright (c) 2015年 LifeisTech. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewController: UIViewController {
+    //クイズを格納する配列
+    var quizArray = [[Any]]()
+    
+    //正解数
+    var correctAnswer: Int = 0
+    
+    //クイズを表示するTextView
+    @IBOutlet var quizTextView: UITextView!
+    
+    //選択肢のボタン
+    @IBOutlet var choiceButtons1: UIButton!
+    @IBOutlet var choiceButtons2: UIButton!
+    @IBOutlet var choiceButtons3: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var tmpArray = [[Any]]()
+        
+        //------------------------ここから下にクイズを書く------------------------//
+        tmpArray.append(["1+3=?", "4", "8", "5", 1])
+        tmpArray.append(["2-6=?", "4", "0", "-4", 3])
+        tmpArray.append(["18÷6=?", "3", "128", "2", 1])
+        tmpArray.append(["16373883×4938274", "80858720697942", "64389202747834", "77028384930200", 1])
+        //------------------------ここから上にクイズを書く------------------------//
+        
+        // 問題をシャッフルしてquizArrayに格納する
+        while (tmpArray.count > 0) {
+            let index = Int(arc4random_uniform(UInt32(tmpArray.count)))
+            quizArray.append(tmpArray[index])
+            tmpArray.remove(at: index)
+        }
+        choiceQuiz()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func choiceQuiz() {
+        quizTextView.text = quizArray[0][0] as? String
+        
+        //選択肢のボタンにそれぞれ選択肢のテキストをセット
+        choiceButtons1.setTitle(quizArray[0][1] as? String, for: .normal)
+        choiceButtons2.setTitle(quizArray[0][2] as? String, for: .normal)
+        choiceButtons3.setTitle(quizArray[0][3] as? String, for: .normal)
+    }
+    
+    @IBAction func choiceAnswer(sender: UIButton) {
+        if quizArray[0][4] as! Int == sender.tag {
+            //正解数を増やす
+            correctAnswer+=1
+        }
+        
+        quizArray.remove(at: 0)
+        //解いた問題数の合計が予め設定していた問題数に達したら結果画面へ
+        if quizArray.count == 0 {
+            performSegueToResult()
+        } else {
+            choiceQuiz()
+        }
+    }
+    
+    func performSegueToResult() {
+        performSegue(withIdentifier: "toResultView", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "toResultView") {
+            
+            let resultView = segue.destination as! ResultViewController
+            resultView.correctAnswer = self.correctAnswer
+        }
+    }
+    
+    
+    
+}
+
+
